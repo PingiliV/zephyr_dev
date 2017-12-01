e2892ab6be5888e12c9e8660b4832fce29ab19c7
vinay pingili <vinaybabu87@gmail.com>
2017-12-01 11:44:11 +0530

    -- test code for rs485

---
+++ samples/drivers/rs485_test/src/test_uart.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief UART cases header file
+ *
+ * Header file for UART cases
+ */
+
+#ifndef __TEST_UART_H__
+#define __TEST_UART_H__
+
+#include <uart.h>
+
+#define UART_DEVICE_NAME CONFIG_UART_CONSOLE_ON_DEV_NAME
+
+
+#endif /* __TEST_UART_H__ */
