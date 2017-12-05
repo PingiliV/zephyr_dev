1137e9672fd6252e709f90decb71786c6935c420
Vinay Babu P <vinaybabu87@gmail.com>
2017-12-05 21:19:23 +0530

    --RS485 update

--- envsetup.sh
+++ envsetup.sh
@@ -1,3 +1,3 @@
-export ZEPHYR_BASE=/home/tcs/00_vinay/Zephyr/GIT_MyWork/zephyr_dev
+export ZEPHYR_BASE=/home/vinay/ZEPHYR/zephyr_dev
 export ZEPHYR_GCC_VARIANT=zephyr
 export ZEPHYR_SDK_INSTALL_DIR=/opt/zephyr-sdk
